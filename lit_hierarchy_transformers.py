b37428ab716455644f946c11bc9845b03e60e4af
[dcastf01] <[dcastf01@gmail.com]>
2021-05-18 18:28:06 +0200

    adicion de solo hacer squeeze en dim=1

--- lit_hierarchy_transformers.py
+++ lit_hierarchy_transformers.py
@@ -37,7 +37,7 @@ class LitHierarchyTransformers(LitSystem):
         def training_step(self,batch,batch_idx):
                 x,targets=batch
                 pre_embbeding,predictions=self.HierarchicalTransformers(x)
-                embbeding=torch.squeeze(self.projection(pre_embbeding))
+                embbeding=torch.squeeze(self.projection(pre_embbeding),dim=1)
 
                 # loss=self.supcons_los(F.normalize(embbeding),targets[-1])
                 loss_crossentropy={}
@@ -61,7 +61,7 @@ class LitHierarchyTransformers(LitSystem):
 
                 total_loss_crossentropy=sum(loss_crossentropy.values())/len(loss_crossentropy)
                 total_loss_contrastive=sum(loss_contrastive.values())/len(loss_contrastive)
-                loss_total=(total_loss_crossentropy+total_loss_contrastive)/2
+                loss_total=total_loss_crossentropy+total_loss_contrastive
                 data_dict={
                         "loss_crossentropy":total_loss_crossentropy,
                         "loss_contrastive":total_loss_contrastive,
@@ -79,7 +79,7 @@ class LitHierarchyTransformers(LitSystem):
                 '''used for logging metrics'''
                 x,targets=batch
                 pre_embbeding,predictions=self.HierarchicalTransformers(x)
-                embbeding=torch.squeeze(self.projection(pre_embbeding))
+                embbeding=torch.squeeze(self.projection(pre_embbeding),dim=1)
                 # loss=self.supcons_los(F.normalize(embbeding),targets[-1])
                 # a=self.con_loss(embbeding,targets[-1])
                 loss={}
@@ -91,12 +91,12 @@ class LitHierarchyTransformers(LitSystem):
                 
                 for (level, y_pred),y_true in zip(predictions.items(),targets):
                         
-                        loss_crossentropy["loss_crosentropy"+level[5:]]=\
+                        loss_crossentropy["val_loss_crosentropy"+level[5:]]=\
                                 self.apply_loss_correction_hierarchical(loss=self.criterion(y_pred,y_true),
                                                                         level=level
                                 )
                         
-                        loss_contrastive["loss_contrastive"+level[5:]]=\
+                        loss_contrastive["val_loss_contrastive"+level[5:]]=\
                                 self.apply_loss_correction_hierarchical(loss=self.contrastive_loss(embbeding,y_true),
                                                                         level=level
                                 )
@@ -105,7 +105,7 @@ class LitHierarchyTransformers(LitSystem):
 
                 total_loss_crossentropy=sum(loss_crossentropy.values())/len(loss_crossentropy)
                 total_loss_contrastive=sum(loss_contrastive.values())/len(loss_contrastive)
-                loss_total=(total_loss_crossentropy+total_loss_contrastive)/2
+                loss_total=total_loss_crossentropy+total_loss_contrastive
                 data_dict={
                         "val_loss_crossentropy":total_loss_crossentropy,
                         "val_loss_contrastive":total_loss_contrastive,
@@ -126,20 +126,33 @@ class HierarchicalTransformers(nn.Module):
         def __init__(self,
                      model_chosen:ModelsAvailable,
                      class_level:dict,
-                     img_size,
+                     img_size:int,
+                     overlap:bool=None,
+                     
                      ):
 
                 super(HierarchicalTransformers,self).__init__()
                 # self.encoder=ViTBase16(class_level=class_level,img_size=img_size)
                 
                 model_name=model_chosen.value
-               
+                overlap=True
                 if model_chosen.name[0:3]==ModelsAvailable.vit_large_patch16_224_in21k.name[0:3]:
                         extras=dict(
-                        img_size=img_size
+                        img_size=img_size,
+                        # embed_layer=
+                        
                         )   
                         self.encoder=timm.create_model(model_name,pretrained=True,**extras)
                         
+                        if overlap is not None:
+                                self.encoder.patch_embed=PatchEmbed(
+                                        img_size=img_size,
+                                        exist_overlap=True,
+                                        slide_step=12)
+
+                                self.encoder.pos_embed = nn.Parameter(torch.zeros(1,
+                                                                                  self.encoder.patch_embed.num_patches + 1,
+                                                                                  768))
 
                         self.heads=nn.ModuleDict(  {
                                 level:nn.Linear(self.encoder.head.in_features,num_classes)
@@ -167,22 +180,36 @@ class HierarchicalTransformers(nn.Module):
                         y[level]=head(x)
                 return x,y
 
-        def forward_features(self,x,extra_tensor=None):
-                B = x.shape[0]
-                x = self.encoder.patch_embed(x)
+
+from timm.models.layers.helpers import to_2tuple
+class PatchEmbed(nn.Module):
+    """ 2D Image to Patch Embedding
+    """
+    def __init__(self, img_size=224, patch_size=16, in_chans=3, embed_dim=768, norm_layer=None,exist_overlap:bool=False,slide_step=None):
+        super().__init__()
+        img_size = to_2tuple(img_size)
+        patch_size = to_2tuple(patch_size)
+        self.img_size = img_size
+        if exist_overlap:
+                self.num_patches = ((img_size[0] - patch_size[0]) // slide_step + 1) * ((img_size[1] - patch_size[1]) // slide_step + 1)
+                self.proj = nn.Conv2d(in_channels=in_chans,
+                                        out_channels=embed_dim,
+                                        kernel_size=patch_size,
+                                        stride=(slide_step, slide_step))
+        else:
                 
-                if extra_tensor is not None:
-                        cls_tokens_from_previuos_net=torch.unsqueeze(extra_tensor,1)
-                        x=torch.cat((cls_tokens_from_previuos_net,x),dim=1)
-                else:
-                        cls_tokens = self.encoder.cls_token.expand(B, -1, -1)  # stole cls_tokens impl from Phil Wang, thanks
-                        x = torch.cat((cls_tokens, x), dim=1)
-                x = x + self.encoder.pos_embed
-                x = self.encoder.pos_drop(x)
+                self.patch_size = patch_size
+                self.grid_size = (img_size[0] // patch_size[0], img_size[1] // patch_size[1])
+                self.num_patches = self.grid_size[0] * self.grid_size[1]
 
-                for blk in self.encoder.blocks:
-                        x = blk(x)
+                self.proj = nn.Conv2d(in_chans, embed_dim, kernel_size=patch_size, stride=patch_size)
+                                      
+        self.norm = norm_layer(embed_dim) if norm_layer else nn.Identity()
 
-                x = self.encoder.norm(x)[:, 0]
-                x = self.encoder.pre_logits(x)
-                return x
+    def forward(self, x):
+        B, C, H, W = x.shape
+        assert H == self.img_size[0] and W == self.img_size[1], \
+            f"Input image size ({H}*{W}) doesn't match model ({self.img_size[0]}*{self.img_size[1]})."
+        x = self.proj(x).flatten(2).transpose(1, 2)
+        x = self.norm(x)
+        return x
