8b5b3fafc62e4b59f2ec01832824b2943afc17df
[dcastf01] <[dcastf01@gmail.com]>
2021-05-19 16:36:29 +0200

    adicion new type variable

--- config.py
+++ config.py
@@ -8,7 +8,10 @@ from typing import Union
 from dataclasses import dataclass,asdict
 
 ROOT_WORKSPACE: str=""
-
+class ArchitectureType(Enum):
+    standar="models normal"
+    hierarchical="loss_con_hierarchical"
+    constrastive="contrastive"
 class ModelsAvailable(Enum):
     hierarchicaltransformers="version_con_multiples_heads"
     vit_base_patch16_224_in21k="vit_base_patch16_224_in21k"
@@ -19,6 +22,7 @@ class ModelsAvailable(Enum):
 class Dataset (Enum):
     grocerydataset=1
     fgvcaircraft=2
+    cars196=3
 class Optim(Enum):
     adam=1
     sgd=2
@@ -39,8 +43,10 @@ class losstype(Enum):
 @dataclass
 class CONFIG(object):
     
-    experiment=ModelsAvailable.hierarchicaltransformers
+    experiment=ModelsAvailable.vit_base_patch16_224_in21k
     experiment_name:str=experiment.name
+    architecture =ArchitectureType.standar
+    architecture_name:str=architecture.name
     # experiment_net:str=experiment.value
     PRETRAINED_MODEL:bool=True
     transfer_learning:bool=False #solo se entrena el head ##no funcional en este repositorio aun
@@ -48,12 +54,12 @@ class CONFIG(object):
     DEVICE: str = "cuda" if torch.cuda.is_available() else "cpu"
     # TRAIN_DIR = "data/train"
     # VAL_DIR = "data/val"
-    batch_size:int =6
-    dataset=Dataset.fgvcaircraft
+    batch_size:int =7
+    dataset=Dataset.cars196
     dataset_name:str=dataset.name
     precision_compute:int=16
     
-    optim=Optim.adam
+    optim=Optim.sgd
     optim_name:str=optim.name
     transform=TransformsAvailable.timm_transforms_imagenet_train
     transform_name:str=transform.name
@@ -61,13 +67,13 @@ class CONFIG(object):
     transform_to_test=TransformsAvailable.transforms_imagenet_eval
     transform_to_test:str=transform_to_test.name
     
-    lr:float = 0.00005
+    lr:float = 0.0028
     AUTO_LR :bool= False
     # LAMBDA_IDENTITY = 0.0
-    NUM_WORKERS:int = 4
+    NUM_WORKERS:int = 0
     SEED:int=1
     IMG_SIZE:int=448
-    NUM_EPOCHS :int= 30
+    NUM_EPOCHS :int= 500
     LOAD_MODEL :bool= True
     SAVE_MODEL :bool= True
     PATH_CHECKPOINT: str= os.path.join(ROOT_WORKSPACE,"classification/model/checkpoint")
@@ -75,7 +81,9 @@ class CONFIG(object):
     gpu0:bool=False
     gpu1:bool=True
     notes:str=" correr diferentes modelos para probar su funcionamiento"
-
+    
+    #hyperparameters
+    
 def create_config_dict(instance:CONFIG):
     return asdict(instance)
 
